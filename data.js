3b84cfb03cb9d681af06d9ca87f65eb6ca92f03a
Jake Antonio Ruiz Zeledón <jackiezeledon@gmail.com>
2020-07-31 13:52:25 -0600

    Update data.js

--- data.js
+++ data.js
@@ -1,48 +1,24 @@
 const main = {
-  name: 'Your Name',
-  mail: 'your.mail@gmail.com',
-  img: 'https://organicthemes.com/demo/profile/files/2018/05/profile-pic.jpg',
-  role: ['Full Stack Developer'],
+  name: 'Jake Ruiz Zeledón',
+  mail: '',
+  img: 'jake.svg',
+  role: ['FrontEnd Developer'],
   // Refer https://ionicons.com/ and put the icon name inside iconName key
   connects: [
     {
-      name: 'Home',
-      iconName: 'logo-firefox',
-      link: 'https://sivanesh-s.github.io'
-    },
-    {
       name: 'Github',
       iconName: 'logo-github',
-      link: 'https://github.com/sivanesh-s'
+      link: 'https://github.com/JackZeled0n'
     },
     {
       name: 'Linkedin',
       iconName: 'logo-linkedin',
-      link: 'https://www.linkedin.com/in/sivanesh-shanmugam/'
-    },
-    {
-      name: 'StackOverflow',
-      iconName: 'logo-stackoverflow',
-      link: 'https://stackoverflow.com/users/6940525/sivanesh-s'
-    },
-    {
-      name: 'Twitter',
-      iconName: 'logo-twitter',
-      link: 'https://twitter.com/sivanesh_fiz'
-    },
-    {
-      name: 'npm',
-      iconName: 'logo-npm',
-      link: 'https://www.npmjs.com/~sivanesh_s'
+      link: 'https://www.linkedin.com/in/jakezeledon/'
     },
     {
       name: 'Mail',
       iconName: 'mail-outline',
-      link: `mailto:${'yourmail@gmail.com'}?Subject=Hello%20again`
+      link: `mailto:${'jackiezeledon@gmail.com'}?Subject=Hello%20again`
     }
-  ],
-  links: [
-    { name: 'Projects', link: '#projects' },
-    { name: 'Blogs', link: '#blogs' }
   ]
 };
